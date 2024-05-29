f0a0c487fb044888b11e2d5dc8f17dd19f0314f1
salmahusna <salmahusna170920@gmail.com>
2024-05-29 09:15:27 +0700

    commit pertama

---
+++ helloworld.php
@@ -0,0 +1,8 @@
+<?php
+namespace salma\kontruksi;
+
+class helloworld {
+    public function sayhello ($name) {
+        return "Hello $name welcome to php and composer to create your own library";
+    }
+}
